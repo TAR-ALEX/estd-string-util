5fa41a4249440960f1d93458b698c4c0bd767b39
Alex Tarasov <71244213+TAR-ALEX@users.noreply.github.com>
2022-09-03 14:57:45 -0600

    Update string_util.h

--- include/estd/string_util.h
+++ include/estd/string_util.h
@@ -4,7 +4,7 @@
 
 namespace estd {
     namespace string_util {
-        std::string ReplaceAll(std::string str, const std::string& from, const std::string& to) {
+        std::string replace_all(std::string str, const std::string& from, const std::string& to) {
             size_t start_pos = 0;
             while ((start_pos = str.find(from, start_pos)) != std::string::npos) {
                 str.replace(start_pos, from.length(), to);
